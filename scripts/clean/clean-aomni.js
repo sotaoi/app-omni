a2a7f3bb936309d6078d4d0bb1ddc8ec85d7ef36
qwertypnk <qwertypnk@protonmail.com>
2021-10-17 08:53:40 +0300

    fix: smallfix

---
+++ scripts/clean/clean-aomni.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const main = async () => {
+  fs.rmdirSync(path.resolve('./deployment'), { recursive: true });
+  fs.rmdirSync(path.resolve('./tmp.deployment'), { recursive: true });
+  fs.mkdirSync(path.resolve('./deployment'));
+  fs.writeFileSync(path.resolve('./deployment/.gitkeep'), '');
+};
+
+main();
